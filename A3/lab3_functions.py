9f414bd1c4dc0f28fb7ff611fecef37235884326
Johanna <johanna.bromark@gmail.com>
2018-03-03 16:55:55 +0100

    started lab 3

---
+++ A3/lab3_functions.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def ml_params(X, labels):
+    """Computes the ML-estimates of micro_k and sigma_k for different classes in dataset X"""
+    micro_k =
+    sigma_k =
+
+    return micro_k, sigma_k
+    
